ea1cf9393c91a1f09f806d0ea2fed9dbbe4b24b1
Guamedo <gonzalopierola@gmail.com>
2019-12-13 11:00:54 +0100

    day 11

--- day11.py
+++ day11.py
@@ -1,4 +1,5 @@
 from utils import execute_program
+import matplotlib.pyplot as plt
 import numpy as np
 
 
@@ -7,15 +8,39 @@ def day11():
         program = [int(n) for n in f.read().split(',')]
     program = program + [0 for _ in range(len(program) + 100)]
 
-    index = 0
-    out, index = execute_program(program, input_list=[0], start_index=index, out_mode=1)
-    print(out)
-    out, index = execute_program(program, input_list=[0], start_index=index, out_mode=1)
-    print(out)
-    out, index = execute_program(program, input_list=[0], start_index=index, out_mode=1)
-    print(out)
-    out, index = execute_program(program, input_list=[0], start_index=index, out_mode=1)
-    print(out)
+    index, out = 0, 0
+    painted_pos = []
+    white_painted = []
+    pos = [0, 0]
+    direction = 0
+    while out != 99:
+        out, index = execute_program(program, input_list=[1 if pos in white_painted else 0], start_index=index, out_mode=1)
+        if out == 99:
+            break
+
+        if out == 0:
+            if pos in white_painted:
+                white_painted.remove(pos.copy())
+        else:
+            if not (pos in white_painted):
+                white_painted.append(pos.copy())
+        if not(pos in painted_pos):
+            painted_pos.append(pos.copy())
+
+        out, index = execute_program(program, start_index=index, out_mode=1)
+        direction = (direction - 1 if out == 0 else direction + 1) % 4
+        print(out)
+        if direction == 0:
+            pos[1] += 1
+        elif direction == 1:
+            pos[0] += 1
+        elif direction == 2:
+            pos[1] -= 1
+        elif direction == 3:
+            pos[0] += 1
+        else:
+            print(f"ERROR: Unknown direction ({direction})")
+    print(len(painted_pos))
 
 
 def day11_star():
